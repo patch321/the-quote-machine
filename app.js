5b1dac8fb0fccd329384a99096850711e66c249c
Patrick <patrickhartleyjr@gmail.com>
2017-03-05 21:17:54 -0600

    fix broken 'get quote' button

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //NEW QUOTE BUTTON SETUP
 var quote_URL = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?';
-var quote = {}
-var getQuote = function(json) {
-	console.log(json)
+var quote = {};
+var getQuote = function (json) {
 	quote = {
 		'quoteText': json.quoteText,
 		'quoteAuthor': json.quoteAuthor
@@ -23,17 +22,16 @@ var getQuote = function(json) {
 //TWEET SETUP
 
 //PAGE LOADS
-$(document).ready(function() {
+$(document).ready(function () {
 	$.getJSON(quote_URL, getQuote, 'jsonp');
-		//GET QUOTE Button Action
-	$("#getQuote-btn").on("click", function() {
+	$("#getQuote-btn").on("click", function () {
 		$.getJSON(quote_URL, getQuote, 'jsonp');
 	});
 		//TWEET Button Action
-	$('.twitter-share-button').on("click", function() {
+	$('.twitter-share-button').on("click", function () {
 		console.log('hello11');
-		var textToTweet = '"' + quote.quoteText + '" - ' + quote.quoteAuthor;
-		var twtLink = 'http://twitter.com/home?status=' + encodeURIComponent(textToTweet);
+		var textToTweet = '"' + quote.quoteText + '" - ' + quote.quoteAuthor,
+            twtLink = 'http://twitter.com/home?status=' + encodeURIComponent(textToTweet);
 		window.open(twtLink, '_blank');
 	});
 	
